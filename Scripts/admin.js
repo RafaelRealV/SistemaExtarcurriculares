7bb1e1d166794bd346d8a69db96e8d56305e5071
Rafael Real <a20050015@unisierra.edu.mx>
2026-04-18 13:29:04 -0700

    diseño panel administrador p1

--- Scripts/admin.js
+++ Scripts/admin.js
@@ -2,43 +2,39 @@
  * Carga usuarios con rol organizador en el select
  */
 function cargarOrganizadores() {
+  const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+  const select = document.getElementById("organizador");
 
-    const usuarios = JSON.parse(localStorage.getItem("usuarios"));
-    const select = document.getElementById("organizador");
+  select.innerHTML = "";
 
-    select.innerHTML = "";
+  const organizadores = usuarios.filter((u) => u.rol === "organizador");
 
-    const organizadores = usuarios.filter(u => u.rol === "organizador");
-
-    organizadores.forEach(org => {
-        select.innerHTML += `
+  organizadores.forEach((org) => {
+    select.innerHTML += `
             <option value="${org.id}">
                 ${org.nombre}
             </option>
         `;
-    });
+  });
 }
 /**
  * Muestra las actividades en la lista
  */
 
 function mostrarActividades() {
+  const actividades = JSON.parse(localStorage.getItem("actividades"));
+  const usuarios = JSON.parse(localStorage.getItem("usuarios"));
 
-    const actividades = JSON.parse(localStorage.getItem("actividades"));
-    const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+  const contenedor = document.getElementById("divListado");
+  contenedor.innerHTML = "";
 
-    const contenedor = document.getElementById("divListado");
-    contenedor.innerHTML = "";
+  actividades.forEach((act) => {
+    const organizador = usuarios.find((u) => u.id === act.id_organizador);
 
-    actividades.forEach(act => {
-
-        const organizador = usuarios.find(u => u.id === act.id_organizador);
-
-        contenedor.innerHTML += `
+    contenedor.innerHTML += `
             <div class="actividad">
                 <a>${act.nombre}</a>
               
-
                 <button onclick="editarActividad(${act.id})">
                     Editar
                 </button>
@@ -48,5 +44,5 @@ function mostrarActividades() {
                 </button>
             </div>
         `;
-    });
+  });
 }
